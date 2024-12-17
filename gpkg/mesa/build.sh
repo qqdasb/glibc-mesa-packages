97525d94d08345119183a7c5d90fd1bc5a0bcb9d
qqdasb <qqdasb@outlook.com>
2024-12-17 20:55:34 +0800

    Update mesa

--- gpkg/mesa/build.sh
+++ gpkg/mesa/build.sh
@@ -3,22 +3,21 @@ TERMUX_PKG_DESCRIPTION="An open-source implementation of the OpenGL specificatio
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_LICENSE_FILE="docs/license.rst"
 TERMUX_PKG_MAINTAINER="@termux-pacman"
-TERMUX_PKG_VERSION="24.2.6"
-TERMUX_PKG_SRCURL=https://mesa.freedesktop.org/archive/mesa-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=2b68c4a6f204c1999815a457299f81c41ba7bf48c4674b0b2d1d8864f41f3709
+TERMUX_PKG_VERSION="24.5.0-dev"
+TERMUX_PKG_SRCURL=git+https://gitlab.freedesktop.org/mesa/mesa.git
+TERMUX_PKG_GIT_BRANCH="main"
 TERMUX_PKG_DEPENDS="libglvnd-glibc, gcc-libs-glibc, libdrm-glibc, libllvm-glibc, libexpat-glibc, zlib-glibc, zstd-glibc, libx11-glibc, libxcb-glibc, libxext-glibc, libxfixes-glibc, libxshmfence-glibc, libxxf86vm-glibc, libwayland-glibc, libvdpau-glibc, libomxil-bellagio-glibc, libva-glibc, libxml2-glibc, libelf-glibc, libbz2-glibc, libclc-glibc"
 TERMUX_PKG_SUGGESTS="mesa-dev-glibc"
-TERMUX_PKG_BUILD_DEPENDS="llvm-glibc, libwayland-protocols-glibc, xorgproto-glibc, glslang-glibc"
+TERMUX_PKG_BUILD_DEPENDS="llvm-glibc, libwayland-protocols-glibc (<= 1.38), xorgproto-glibc, glslang-glibc, libwayland-glibc"
 TERMUX_PKG_PYTHON_COMMON_DEPS="mako, setuptools, pyyaml"
 # disabling libunwind, microsoft-clc and valgrind will improve performance
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -D android-libbacktrace=disabled
--D b_ndebug=true
 -D dri3=enabled
 -D egl=enabled
 -D gallium-opencl=icd
--D gallium-drivers=freedreno,swrast,virgl,zink,r600,radeonsi,nouveau,lima,panfrost
+-D gallium-drivers=freedreno,swrast,virgl,zink,lima,panfrost
 -D gallium-extra-hud=true
 -D gallium-nine=true
 -D gallium-va=enabled
@@ -38,12 +37,12 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -D platforms=x11,wayland
 -D shared-glapi=enabled
 -D valgrind=disabled
--D vulkan-layers=device-select,overlay
+-D vulkan-layers=device-select,overlay,screenshot
 "
 
 termux_step_pre_configure() {
 	case $TERMUX_ARCH in
-		arm|aarch64) TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" -Dvulkan-drivers=swrast,panfrost,freedreno -Dfreedreno-kmds=msm,kgsl";;
+		arm|aarch64) TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" -Dvulkan-drivers=swrast,panfrost,freedreno -Dfreedreno-kmds=msm,kgsl,virtio,wsl";;
 		*) TERMUX_PKG_EXTRA_CONFIGURE_ARGS+=" -Dvulkan-drivers=swrast";;
 	esac
 	export MESON_PACKAGE_CACHE_DIR="${TERMUX_PKG_SRCDIR}"
